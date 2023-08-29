edf06197968b0b23db302beb38e087dfeeca7382
monicalopez18 <monicalopez0427@gmail.com>
2023-08-29 17:59:04 -0500

    funciones con promise

--- index.js
+++ index.js
@@ -8,48 +8,64 @@ const rl = readline.createInterface({
 const tareas = [];
 
 function mostrarTareas() {
-  console.log('Lista de tareas:');
-  tareas.forEach((tarea, index) => {
-    console.log(`${index + 1}. [${tarea.estado ? 'X' : ' '}] - ${tarea.descripcion}`);
+  return new Promise((resolve, reject) => {
+    console.log('Lista de tareas:');
+    tareas.forEach((tarea, index) => {
+      console.log(`${index + 1}. [${tarea.estado ? 'X' : ' '}] - ${tarea.descripcion}`);
+    });
+    resolve();
   });
 }
 
 function agregarTarea() {
-  rl.question('Ingrese la descripción de la tarea: ', (descripcion) => {
-    tareas.push({ descripcion, estado: false });
-    console.log('Tarea agregada.');
-    mostrarTareas();
-    rl.prompt();
+  return new Promise((resolve, reject) => {
+    rl.question('Ingrese la descripción de la tarea: ', (descripcion) => {
+      tareas.push({ descripcion, estado: false });
+      console.log('Tarea agregada.');
+      resolve();
+    });
   });
 }
 
 function eliminarTarea() {
-  mostrarTareas();
-  rl.question('Ingrese el número de la tarea que desea eliminar: ', (numero) => {
-    const index = parseInt(numero) - 1;
-    if (index >= 0 && index < tareas.length) {
-      tareas.splice(index, 1);
-      console.log('Tarea eliminada.');
-    } else {
-      console.log('Número de tarea inválido.');
-    }
-    mostrarTareas();
-    rl.prompt();
+  return new Promise((resolve, reject) => {
+    mostrarTareas()
+      .then(() => {
+        rl.question('Ingrese el número de la tarea que desea eliminar: ', (numero) => {
+          const index = parseInt(numero) - 1;
+          if (index >= 0 && index < tareas.length) {
+            tareas.splice(index, 1);
+            console.log('Tarea eliminada.');
+          } else {
+            console.log('Número de tarea inválido.');
+          }
+          resolve();
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 }
 
 function completarTarea() {
-  mostrarTareas();
-  rl.question('Ingrese el número de la tarea que desea marcar como completada: ', (numero) => {
-    const index = parseInt(numero) - 1;
-    if (index >= 0 && index < tareas.length) {
-      tareas[index].estado = true;
-      console.log('Tarea completada.');
-    } else {
-      console.log('Número de tarea inválido.');
-    }
-    mostrarTareas();
-    rl.prompt();
+  return new Promise((resolve, reject) => {
+    mostrarTareas()
+      .then(() => {
+        rl.question('Ingrese el número de la tarea que desea marcar como completada: ', (numero) => {
+          const index = parseInt(numero) - 1;
+          if (index >= 0 && index < tareas.length) {
+            tareas[index].estado = true;
+            console.log('Tarea completada.');
+          } else {
+            console.log('Número de tarea inválido.');
+          }
+          resolve();
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 }
 
@@ -63,29 +79,79 @@ function mostrarMenu() {
   rl.prompt();
 }
 
-rl.on('line', (line) => {
-  const opcion = parseInt(line.trim());
-  switch (opcion) {
-    case 1:
-      mostrarTareas();
-      break;
-    case 2:
-      agregarTarea();
-      break;
-    case 3:
-      eliminarTarea();
-      break;
-    case 4:
-      completarTarea();
-      break;
-    case 5:
-      rl.close();
-      break;
-    default:
-      console.log('Opción inválida.');
-      mostrarMenu();
-  }
-});
+function obtenerOpcion() {
+  return new Promise((resolve, reject) => {
+    rl.on('line', (line) => {
+      const opcion = parseInt(line.trim());
+      if (isNaN(opcion)) {
+        reject(new Error('Opción inválida.'));
+      } else {
+        resolve(opcion);
+      }
+    });
+  });
+}
 
-console.log('Bienvenido a la Gestión de Tareas.');
-mostrarMenu();
+function iniciarGestionTareas() {
+  return new Promise((resolve, reject) => {
+    console.log('Bienvenido a la Gestión de Tareas.');
+    mostrarMenu();
+    obtenerOpcion()
+      .then((opcion) => {
+        switch (opcion) {
+          case 1:
+            mostrarTareas()
+              .then(() => {
+                iniciarGestionTareas();
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+                iniciarGestionTareas();
+              });
+            break;
+          case 2:
+            agregarTarea()
+              .then(() => {
+                iniciarGestionTareas();
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+                iniciarGestionTareas();
+              });
+            break;
+          case 3:
+            eliminarTarea()
+              .then(() => {
+                iniciarGestionTareas();
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+                iniciarGestionTareas();
+              });
+            break;
+          case 4:
+            completarTarea()
+              .then(() => {
+                iniciarGestionTareas();
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+                iniciarGestionTareas();
+              });
+            break;
+          case 5:
+            rl.close();
+            break;
+          default:
+            console.log('Opción inválida.');
+            iniciarGestionTareas();
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        iniciarGestionTareas();
+      });
+  });
+}
+
+iniciarGestionTareas();
