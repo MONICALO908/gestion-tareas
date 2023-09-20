5f78b0d196c2c9a3b2c67a9d7e3519835055f135
monicalopez18 <monicalopez0427@gmail.com>
2023-09-19 19:38:38 -0500

    Servidor Lista de Tareas Express Router

---
+++ review-1.md
@@ -0,0 +1,18 @@
+Producto: Aplicación de Lista de Tareas "TaskList"
+¿Qué es mi producto y para qué sirve?
+TaskList es una aplicación de lista de tareas diseñada para ayudarte a mantener tu vida organizada y ser más productivo.
+
+¿Cuáles son las funcionalidades más importantes y por qué los usuarios las usarían?
+Crear Listas de Tareas: Organiza tus pendientes en listas personalizadas.
+Agregar Tareas Fácilmente: Añade tareas rápidamente con unos pocos toques.
+Recordatorios y Notificaciones: Recibe alertas para no olvidar tareas importantes.
+Colaboración: Comparte listas con otros para proyectos compartidos.
+Acceso Múltiple Dispositivos: Administra tus tareas desde cualquier lugar.
+Interfaz Amigable: Fácil de usar para todos.
+Histórico de Tareas: Realiza un seguimiento de tu progreso y logros.
+TaskList te ayuda a ser más organizado, productivo y a cumplir tus compromisos sin esfuerzo.
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const http = require('http');
-const host = '127.0.0.1';
+const express = require('express');
+const server = express();
+// const host = '127.0.0.1';
 const port = 3000;
+const listViewRouter = require('./list-view-router');
+const listEditRouter = require('./list-edit-router');
 
-const tasks = [
-  { id: 1, description: 'Hacer la compra', completed: false },
-  { id: 2, description: 'Estudiar Node.js', completed: true },
-  { id: 3, description: 'Hacer ejercicio', completed: false },
-];
+// const tasks = [
+//   { id: 1, description: 'Hacer la compra', completed: false },
+//   { id: 2, description: 'Estudiar Node.js', completed: true },
+//   { id: 3, description: 'Hacer ejercicio', completed: false },
+// ];
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/tasks' && req.method === 'GET') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(tasks));
-  } else {
-    res.statusCode = 404;
-    res.end('Ruta no encontrada');
-  }
+
+server.get('/', (req, res) => {
+  res.send('Hola mundo');
 });
+server.use('/list-view', listViewRouter);
+server.use('/list-edit', listEditRouter);
+
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/tasks' && req.method === 'GET') {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.end(JSON.stringify(tasks));
+//   } else {
+//     res.statusCode = 404;
+//     res.end('Ruta no encontrada');
+//   }
+// });
 
 server.listen(port, () => {
   console.log(`Servidor en funcionamiento en http://localhost:${port}`);
 });
+
+module.exports = server
